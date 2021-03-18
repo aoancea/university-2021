c1ff811f3fd08b1b8bea412d182fb1ab5129ff74
Alexandru Oancea <alx.oancea@gmail.com>
2021-03-18 11:06:42 +0200

    - week 4

---
+++ week-04/WebApplication1/WebApplication1/Migrations/20210318085643_Add_Column_DateOfBirth_In_Table_Duckbill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class Add_Column_DateOfBirth_In_Table_Duckbill : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Duckbill",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Duckbill");
+        }
+    }
+}
---
+++ week-04/WebApplication1/WebApplication1/Models/Duckbill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class Duckbill
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
