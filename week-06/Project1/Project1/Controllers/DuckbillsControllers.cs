ce359a974b69ff91a82a746f9ab0e69b0d7da373
Alexandru Oancea <alx.oancea@gmail.com>
2021-04-01 11:56:42 +0300

    week-06: initial project

---
+++ week-06/Project1/Project1/Controllers/DuckbillsControllers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DuckbillsControllers : ControllerBase
+    {
+        private readonly ILogger<DuckbillsControllers> _logger;
+
+        public DuckbillsControllers(ILogger<DuckbillsControllers> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Models.Duckbill> Get()
+        {
+            return new Models.Duckbill[2]
+            {
+                new Models.Duckbill() { ID = Guid.NewGuid(), Name = "Duckbill 1" },
+                new Models.Duckbill() { ID = Guid.NewGuid(), Name = "Duckbill 2" }
+            };
+        }
+    }
+}
